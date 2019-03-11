51c8e27a89688072fb36f86882c9deea0668fee9
Zuzana Drozdová <droz00@vse.cz>
2019-03-11 22:54:53 +0100

    listView

--- src/main/java/droz00/adventure/Main.java
+++ src/main/java/droz00/adventure/Main.java
@@ -1,5 +1,6 @@
 package droz00.adventure;
 
+import droz00.adventure.gui.SceneController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,16 +10,29 @@ import javafx.stage.Stage;
 public class Main extends Application {
     public static void main(String[] args) {
 
-        //TerminalUI terminal = new TerminalUI();
-        //terminal.play();
-        launch(args);
 
+        if (args.length > 0) {
+            if (args[0].equals("text")) {
+                TerminalUI terminal = new TerminalUI();
+                terminal.play();
+
+            } else {
+                System.out.println("Invalid parameter, \n please type 'text' to start the user interface");
+            }
+        } else {
+
+            //TerminalGUI terminalGUI = new TerminalGUI();
+            //terminalGUI.play();
+            launch(args);
+
+        }
     }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/scene.fxml"));
-        Scene scene = new Scene(root,1280,720);
+        Scene scene = new Scene(root, 1280, 720);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
--- src/main/java/droz00/adventure/gui/SceneController.java
+++ src/main/java/droz00/adventure/gui/SceneController.java
@@ -1,16 +1,46 @@
 package droz00.adventure.gui;
 
 
+import com.sun.tools.javac.jvm.Items;
+import droz00.adventure.Game;
+import droz00.adventure.places.Place;
+import javafx.beans.Observable;
+import javafx.beans.value.ObservableMapValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 
-public class SceneController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SceneController implements Initializable {
 
     @FXML
     private TextField textField;
 
-    public void onButtonClick(){
-        System.out.println(textField.getText());
+    public String onButtonClick() {
+        String command = textField.getText();
+        System.out.println(command);
+        textField.clear();
+        return command;
+    }
+
+    @FXML
+    private ListView<String> listView1;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Game game = new Game();
+        ObservableList<String> data = FXCollections.observableArrayList(game.getCurrentPlace().getFiles().keySet());
+        listView1.setItems(data) ;
+
+
 
     }
 }
